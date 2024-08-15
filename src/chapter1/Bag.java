d934876a62730752fc1beaaf1c5b67049bdf2554
unknown <elwl5515@gmail.com>
2024-08-15 23:29:31 +0900

    📝 Study: Object

---
+++ src/chapter1/Bag.java
@@ -0,0 +1,47 @@
+package chapter1;
+
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
+    private void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    private boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    private void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+
+    public Bag(Long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, Long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+}
